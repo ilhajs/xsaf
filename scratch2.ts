997a787e070e59a17fed5435775026af8addef9a
Ryuz <ryuzer@proton.me>
2026-08-03 19:56:19 +0200

    chore(repo): clean up

--- scratch2.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "bun:test";
-import { ORPCError } from "@orpc/server";
-
-test("throws", () => {
-  expect(() => {
-    throw new ORPCError("UNAUTHORIZED");
-  }).toThrow("UNAUTHORIZED");
-});
